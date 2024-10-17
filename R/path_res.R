3ba102d3fbd6fcd7312a2043ddd6218fb2348d36
Margelos Theodoros <ted.margelos02@gmail.com>
2024-10-17 21:45:22 +0300

    assign basenames of group paths as names, create dataspace and write it in an excel

--- R/path_res.R
+++ R/path_res.R
@@ -1,18 +1,19 @@
-user_inputs <- function(path_for_group1, path_for_group2) {
-g1.name <- basename(path_for_group1)
-g2.name <- basename(path_for_group2)
-
-
-
-   dataspace <- data.frame()
+user_inputs <- function(group1, group2)
+  {
+g1.name <- basename(group1)
+g2.name <- basename(group2)
+#group number is now 2
+groups_number <- 2
+#create the dataspace for all the data
+dataspace <- data.frame()
+#assign the excel files to a list
 file_names_g1<-list.files(path=group1,pattern="*.xlsx")
+
 setwd(group1)
 for (i in 1:length(file_names_g1)) {
-  file_case <- read_excel(paste(group1,file_names_g1[i],sep = "/"), sheet = 1)
+  file_case <- readxl::read_excel(paste(group1,file_names_g1[i],sep = "/"), sheet = 1)
   dataspace <- rbind(dataspace,file_case[,1:2])
 }
-
-# group 2 protein IDs
 if (groups_number == 2 | groups_number == 3 | groups_number == 4 | groups_number==5 | groups_number==6 | groups_number==7 | groups_number==8 | groups_number==9){
   file_names_g2<-list.files(path=group2,pattern="*.xlsx")
   setwd(group2)
@@ -25,5 +26,7 @@ if (groups_number == 2 | groups_number == 3 | groups_number == 4 | groups_number
   if (!dir.exists(path_res)) {
     stop("The specified folder does not exist.")
   }
+    openxlsx::write.xlsx(meano_data, file = "meano_data.xlsx")
+    message("the excel was created")
 }
 
