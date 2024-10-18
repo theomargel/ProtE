c8b9189e7714cc5c96da82e176172e90114aed81
Margelos Theodoros <ted.margelos02@gmail.com>
2024-10-18 12:11:00 +0300

    create proteomic list , adding area and normalizing

--- R/path_res.R
+++ R/path_res.R
@@ -2,7 +2,7 @@
 #'
 #' Creates an a excel file
 #'
-#' @param group1  path to the folder where the samples from group 2 are
+#' @param group1  path to the folder where the samples from group 1 are
 #' @param group2 path to the folder where the samples from group 2 are
 #'
 #'
@@ -39,12 +39,52 @@ if (groups_number == 2 | groups_number == 3 | groups_number == 4 | groups_number
     dataspace <- rbind(dataspace,file_case[,1:2])
   }
 }
+dataspace <- dataspace[!is.na(dataspace$Accession),]
+dupl <- duplicated(dataspace[,1])
+dataspace_no_dupl <- dataspace[!dupl,]
+dataspace_no_dupl <- droplevels(dataspace_no_dupl)
+ML <- data.frame(dataspace_no_dupl, stringsAsFactors = FALSE)
+### Merge all areas from the files
+dataspace <- dataspace_no_dupl
+
+setwd(group1)
+for (i in 1:length(file_names_g1)) {
+  file_case <- readxl::read_excel(paste(group1,file_names_g1[i],sep = "/"), sheet = 1)
+  file_case[file_case==0] <-1
+  file_case[is.na(file_case)]<-0
+  dataspace <- merge(x = dataspace,y = file_case[,c(1,9)], by = "Accession" ,all.x = TRUE)
+  colnames(dataspace)[length(colnames(dataspace))] <- file_names_g1[i]
+}
+
+
+##Add area from group 2
+if (groups_number == 2 | groups_number == 3 | groups_number == 4 | groups_number == 5 | groups_number==6 | groups_number==7 | groups_number==8 | groups_number==9){
+  setwd(group2)
+  for (i in 1:length(file_names_g2)) {
+    file_case <- readxl::read_excel(paste(group2,file_names_g2[i],sep = "/"), sheet = 1)
+    file_case[file_case==0] <-1
+    file_case[is.na(file_case)]<-0
+    dataspace <- merge(x = dataspace,y = file_case[,c(1,9)], by = "Accession" ,all.x = TRUE)
+    colnames(dataspace)[length(colnames(dataspace))] <- file_names_g2[i]
+  }
+}
+
+colnames(dataspace) <- gsub(".xlsx", "", colnames(dataspace))
+
+
     path_res <- readline(prompt = "Specify folder where you want to save the results: e.g.: C:/Users/User/Documents/")
   if (!dir.exists(path_res)) {
     stop("The specified folder does not exist.")
   }
     setwd(path_res)
-    openxlsx::write.xlsx(dataspace, file = "dataspace.xlsx")
-    message("the excel was created")
+    openxlsx::write.xlsx(dataspace, file = "Masterlist.xlsx")
+    message("the excel of the list with all proteomics data was created as Masterlist.xlsx")
+    #normalize PPm
+    dataspace[, -1:-2] <- lapply(dataspace[, -1:-2], function(x) {
+      sum_x <- sum(x, na.rm = TRUE)  # Sum of the column, ignoring NAs
+      ifelse(is.na(x), 0, (x / sum_x) * 10^6)  # NA=0 , normalize the rest
+    })
+    openxlsx::write.xlsx(dataspace, file = "normalized_list.xlsx")
+    message("the excel of the normalized list was created")
 }
 
