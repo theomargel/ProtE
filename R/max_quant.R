32ec384f87f7ced29f5128a9c0e781d43cd2bdf3
RafStrog <68844518+RafStrog@users.noreply.github.com>
2024-11-08 17:22:11 +0200

    Removed clustering of heatmap columns

--- R/max_quant.R
+++ R/max_quant.R
@@ -577,7 +577,7 @@ max_quant <- function(excel_file,
     mycols <- grDevices::colorRampPalette(c("blue", "white", "red"))(100)
     heatmap_data<- ComplexHeatmap::Heatmap(as.matrix(zlog.dataspace.sig),
                                            cluster_rows = TRUE,
-                                           cluster_columns = TRUE ,
+                                           cluster_columns = FALSE,
                                            show_row_names = FALSE,
                                            show_column_names = FALSE,
                                            column_split = groups_list_f,
